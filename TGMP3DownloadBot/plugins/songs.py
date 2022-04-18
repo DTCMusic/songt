91b4bc87349f569cee3da90e0558309fc2fbbda5
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-04-19 00:32:44 +0400

    Update songs.py

--- TGMP3DownloadBot/plugins/songs.py
+++ TGMP3DownloadBot/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '👉 TG MP3 Download Bot 👈 \n~ @fastsongdownloderslbzbot ⭕️'
+        rep = '📥 TG Manhı yükləyən Bot 📥 @LedySongBot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
