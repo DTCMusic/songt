695d36e8a2b4cb97f7055d02a54125d987a13a8e
DTCMusic <89629350+DTCMusic@users.noreply.github.com>
2022-06-17 02:36:12 +0400

    Update songs.py

--- TGMP3DownloadBot/plugins/songs.py
+++ TGMP3DownloadBot/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '📥 TG Manhı yükləyən Bot 📥 @LedySongBot'
+        rep = '📥 TG Manhı yükləyən Bot 📥 @karabakhSongBot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
